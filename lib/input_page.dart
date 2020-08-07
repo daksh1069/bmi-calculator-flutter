c1d8dd1b6598ffe23decc195cf84f97ca6a07a64
daksh <dakshdhul@gmail.com>
2020-08-07 23:11:30 +0530

    Before Adding the Functionality

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -216,11 +216,21 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          Container(
-            color: kBottomContainerColor,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, '/result');
+            },
+            child: Container(
+              child: Center(
+                child: Text(
+                  'CALCULATE',
+                ),
+              ),
+              color: kBottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           ),
         ],
       ),
